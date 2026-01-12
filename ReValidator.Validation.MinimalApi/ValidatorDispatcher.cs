48ced86f396ea0aa2134de35fda7adab8ede3be2
Vlad Ganusceac <vlad.ganuscheak@gmail.com>
2026-01-12 22:39:31 +0200

    Added package for validating using ReValidator on MVC actions using custom action filter

---
+++ ReValidator.Validation.MinimalApi/ValidatorDispatcher.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Linq.Expressions;
+
+namespace ReValidator.Validation.MinimalApi
+{
+    public static class ValidatorDispatcher
+    {
+        private static readonly ConcurrentDictionary<Type, Func<object, object, ValidationResult>> _cache
+            = new();
+
+        public static ValidationResult Dispatch(object validator, object value)
+        {
+            var modelType = value.GetType();
+
+            var invoker = _cache.GetOrAdd(modelType, CreateInvoker);
+
+            return invoker(validator, value);
+        }
+
+        private static Func<object, object, ValidationResult> CreateInvoker(Type modelType)
+        {
+            var validatorType = typeof(IValidator<>).MakeGenericType(modelType);
+
+            var validatorParam = Expression.Parameter(typeof(object), "validator");
+            var valueParam = Expression.Parameter(typeof(object), "value");
+
+            var castValidator = Expression.Convert(validatorParam, validatorType);
+            var castValue = Expression.Convert(valueParam, modelType);
+
+            var call = Expression.Call(
+                castValidator,
+                validatorType.GetMethod("Validate")!,
+                castValue);
+
+            var lambda = Expression.Lambda<Func<object, object, ValidationResult>>(
+                call,
+                validatorParam,
+                valueParam);
+
+            return lambda.Compile();
+        }
+    }
+}
---
+++ ReValidator.Validation.Mvc/ValidationResultExtensions.cs
@@ -0,0 +1,13 @@
+﻿namespace ReValidator.Validation.Mvc
+{
+    public static class ValidationResultExtensions
+    {
+        public static ReValidationProblem ToProblem(this ValidationResult result)
+        {
+            return new ReValidationProblem
+            {
+                Errors = result.Errors.ToDictionary(x => x.PropertyName, x => x.ErrorMessages)
+            };
+        }
+    }
+}
